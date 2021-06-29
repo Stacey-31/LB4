406d792a00e1ae628e2e9a4196f6ebdb094ada2c
Stacey31 <53440572+Stacey-31@users.noreply.github.com>
2021-06-29 22:05:25 +0000

    suff

--- main.py
+++ main.py
@@ -28,27 +28,56 @@ print(triangle_area(8,11)) #would return 44 and print to console
             #goodToGo = False
 
 #StepIV
-shoeInv = {"Jordan13": 10, "Yeezy": 80, "Foamposite": 10, "Airmax": 5, "SB Dunk":20}
-print(shoeInv["SB Dunk"])
+#shoeInv = {"Jordan13": 10, "Yeezy": 80, "Foamposite": 10, "Airmax": 5, "SB Dunk":20}
+#print(shoeInv["SB Dunk"])
 
-def clearanceSale(d, updater):
-  d1 = {"Jordan13": d["Jordan13"] /updater }
+#def clearanceSale(d, updater):
+  #d1 = {"Jordan13": d["Jordan13"] /updater }
   #shoeInv.update("Jordan13", shoeInv["Jordan13"] /updater)
-  d.update( d1)
+  #d.update( d1)
 
  
-  d2 = {"Yeezy": d["Yeezy"] /updater }
-  d.update( d2)
+  #d2 = {"Yeezy": d["Yeezy"] /updater }
+  #d.update( d2)
   #(shoeInv["Foamposite"] /updater)
-  d3 = {"Foamposite": d["Foamposite"] /updater }
-  d.update( d3)
+  #d3 = {"Foamposite": d["Foamposite"] /updater }
+  #d.update( d3)
   #(shoeInv["Airmax"] /updater)
-  d4 = {"Airmax": d["Airmax"] /updater }
-  d.update( d4)
+  #d4 = {"Airmax": d["Airmax"] /updater }
+  #d.update( d4)
   #(shoeInv["SB Dunk"] /updater)
-  d5 = {"SB Dunk": d["SB Dunk"] /updater }
-  d.update( d5)
+  #d5 = {"SB Dunk": d["SB Dunk"] /updater }
+  #d.update( d5)
 
-  return (d)
+  #return (d)
 
-print(clearanceSale(shoeInv,5)) #answer = 4
+#print(clearanceSale(shoeInv,5)) #answer = 4
+
+#STepVI
+nba_player = {"Curry" : 30, "Lebron": 23, "Duran": 7, "Oubre": 12, "Harden": 13}
+
+def largestJerseyNumber(nba_player) :  
+    largestjerseyNum = 0
+
+    for x in range(5):
+    #if (nba_player["Curry"]) > (nba_player["Lebron"] ):
+    print("x is", x)
+
+    print( nba_player["Curry"])
+    if (nba_player["Curry"]) > (nba_player["Lebron"] ):
+        largestjerseyNum = nba_player["Curry"]
+
+    if (nba_player["Curry"]) > (nba_player["Lebron"] ):
+        largestjerseyNum = nba_player["Curry"]
+      else:
+            largestjerseyNum = nba_player["Duran"]
+
+    print (largestjerseyNum)
+  #read in 1sr jersey number, 
+
+
+  #compare to 2nd jersey number
+  #if 1st > 2nd
+  #largest = 1st
+
+
